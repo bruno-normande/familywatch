363099f27bd66e54424ae95f7cf09a7f6bb06cac
Bruno Normande <normandelins@gmail.com>
2018-10-23 03:08:20 +0000

    Adicionando projeto inicial criado por SPRING INITIALIZR

---
+++ src/main/java/com/profbruno/familywatch/FamilywatchApplication.java
@@ -0,0 +1,12 @@
+package com.profbruno.familywatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilywatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilywatchApplication.class, args);
+	}
+}
