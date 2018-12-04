f8057e495d26006466b896fb115ea1cc3f2a6168
Bruno Normande <normandelins@gmail.com>
2018-12-04 20:51:31 -0300

    Fetch! :)

--- src/main/resources/static/js/mylist.js
+++ src/main/resources/static/js/mylist.js
@@ -1,83 +1,58 @@
 
-
 function clique(){
 	let lat = document.getElementById("lat").value;
 	let long = document.getElementById("long").value;
 	let tm = document.getElementById("tm").value;
 	
-//	let data = {"timestamp": tm, 
-//				"coordinate": {
-//					"latitude":lat, 
-//					"longitude": long}, 
-//				"phone": {"id": 20}
-//				};
+	let data = {"timestamp": tm, 
+				"coordinate": {
+					"latitude":lat, 
+					"longitude": long}, 
+				"phone": {"id": 69}
+				};
 	
-	axios.post("/position", {"timestamp": tm, 
-							"coordinate": {
-								"latitude":lat, 
-								"longitude": long}, 
-							"phone": {"id": 20}
-		})
-		.then(createTable)
-		.catch(function(error){
-			console.log(error);
-		});
-	
-//	let xhr2 = new XMLHttpRequest();
-//	xhr2.open('POST', '/position');
-//	xhr2.onload = createTable;
-//	xhr2.setRequestHeader("Content-Type", "application/JSON")
-//	xhr2.send(JSON.stringify(data));
-	
+	fetch("/position", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/JSON"
+		},
+		body: JSON.stringify(data)
+		
+	}).then(function(response){
+		createTable();
+		
+	}).catch(function(error){
+		console.log(error);
+	})
 	
 }
 
 function createTable(){
 	
-	axios.get("/position", {params: {page:1}})
+	fetch("/position")
 		.then(function(response){
-			console.log(response);
-			let tb = document.getElementById("posicoes");
-			tb.innerHTML = "<tr><th>Celular</th><th>Latitude</th><th>Longitude</th><th>Timestamp</th></tr>";
-			for(let i=0; i < response.data.content.length; i++){
-				let p = response.data.content[i];
-				if(p.coordinate)
-					tb.innerHTML += `<tr><th>${p.phone.number}</th><th>${p.coordinate.latitude}</th><th>${p.coordinate.longitude}</th><th>${p.timestamp}</th></tr>`;
+			
+			if(response.status >= 200 && response.status <= 300){
+				
+				response.json()
+					.then(function(data){
+					
+						let tb = document.getElementById("posicoes");
+						tb.innerHTML = "<tr><th>Celular</th><th>Latitude</th><th>Longitude</th><th>Timestamp</th></tr>";
+						
+						for(let i=0; i < data.content.length; i++){
+							let p = data.content[i];
+							if(p.coordinate)
+								tb.innerHTML += `<tr><th>${p.phone.number}</th><th>${p.coordinate.latitude}</th><th>${p.coordinate.longitude}</th><th>${p.timestamp}</th></tr>`;
+						}
+				})
+				
+				
 			}
 			
-		})
-		.catch(function(error){
+		}).catch(function(error){
 			console.log(error);
-		});
-	
-	
-//	let xhr2 = new XMLHttpRequest();
-//	xhr2.open('GET', '/position');
-//
-//	xhr2.onload = function() {
-//		if(this.status == 200){
-//			
-//			let r = JSON.parse(this.responseText);
-//			console.log(r);
-//			
-//			let tb = document.getElementById("posicoes");
-//			
-//			//tb.innerHTML = "<tr><th>Celular</th><th>Latitude</th><th>Longitude</th><th>Timestamp</th></tr>";
-//			
-//			for(let i=0; i < r.content.length; i++){
-//				let p = r.content[i];
-//				if(p.coordinate)
-//					tb.innerHTML += `<tr><th>${p.phone.number}</th><th>${p.coordinate.latitude}</th><th>${p.coordinate.longitude}</th><th>${p.timestamp}</th></tr>`;
-//			}
-//			
-////			r.content.forEach(p => {
-////				if(p.coordinate)
-////				tb.innerHTML += `<tr><th>${p.phone.number}</th><th>${p.coordinate.latitude}</th><th>${p.coordinate.longitude}</th><th>${p.timestamp}</th></tr>`;
-////			});
-//		}
-//	};
-//
-//	xhr2.send();
+		});	
 }
 
 createTable();
